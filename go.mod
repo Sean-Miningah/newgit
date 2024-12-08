598af627af3d5ecc33f2edd68690868f14b5d2fd
sean-miningah <seanpminingah@gmail.com>
2024-12-08 06:29:14 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Sean-Miningah/newgit
+
+go 1.23.2
---
+++ newgit.go
@@ -0,0 +1,8 @@
+package newgit
+
+import "fmt"
+
+func Hello(name string) string {
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message
+}
